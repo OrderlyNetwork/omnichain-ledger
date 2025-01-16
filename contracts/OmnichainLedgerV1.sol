9974b481a9ab7204c7e5beff2277115d0049357e
Dmitry Blinov <dmitry@orderly.network>
2025-01-16 12:07:39 +0200

    Update OmnichainLedgerTestV1 for dev env

--- contracts/OmnichainLedgerV1.sol
+++ contracts/OmnichainLedgerV1.sol
@@ -33,7 +33,7 @@ contract OmnichainLedgerV1 is LedgerAccessControl, UUPSUpgradeable, ChainedEvent
     }
 
     function VERSION() external pure virtual returns (string memory) {
-        return "1.0.5";
+        return "1.0.6";
     }
 
     /* ====== UUPS AUTHORIZATION ====== */
