2afd7e1c27df8ef6f0c00a97aa17464b7d7c490a
Dmitry Blinov <dmitry@orderly.network>
2025-02-26 17:53:14 +0200

    Fix LedgerSolana tests

--- test/forge/mocks/OappMock.sol
+++ test/forge/mocks/OappMock.sol
@@ -2,11 +2,18 @@
 pragma solidity ^0.8.20;
 
 import {OCCLedgerMessage} from "../../../contracts/lib/OCCTypes.sol";
+import {MessagingFee} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OApp.sol";
 
 contract OappMock {
     event LedgerOappSend();
+    event LedgerOappSendQuoted();
 
-    function ledgerOappSend(OCCLedgerMessage calldata) external {
+    function ledgerOappSend(OCCLedgerMessage calldata) external payable {
         emit LedgerOappSend();
+    }
+
+    function ledgerOappSendQuote(OCCLedgerMessage calldata) external returns (MessagingFee memory) {
+        emit LedgerOappSendQuoted();
+        return MessagingFee(1_000_000, 0);
     }
 }
